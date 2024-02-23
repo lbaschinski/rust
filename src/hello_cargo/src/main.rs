088e5fc56df5ef4b33f8e8a5850fa245385dc2b2
Lucas Baschinski <12460927+lbaschinski@users.noreply.github.com>
2024-02-23 11:11:30 +0100

    Update hello_cargo with comments

--- src/hello_cargo/src/main.rs
+++ src/hello_cargo/src/main.rs
@@ -1,3 +1,13 @@
+//! Doc comment that is parsed into HTML library documentation for the enclosed item...
+/// Doc comment only for the following item:
 fn main() {
-    println!("Hello, world!");
+    println!("Hello World!");
+    // Normal line comment that is not parsed into HTML
+    /*
+    Normal block comment that is not parsed into HTML
+    */
+    println!("This is Rust!");
+    // You can use block comments in the middle of statements!
+    let x = 5 + /* 90 + */ 5;
+    println!("Todays number is 10 right? x = {}", x);
 }
